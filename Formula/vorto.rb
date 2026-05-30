ac82eb07170c262de6a7bf88f4b669cbb287a48f
kistos <cistus.0621@gmail.com>
2026-05-30 18:54:21 +0900

    DRY version + livecheck for vorto formula

--- Formula/vorto.rb
+++ Formula/vorto.rb
@@ -1,22 +1,29 @@
 class Vorto < Formula
+  VORTO_VERSION = "0.14.0".freeze
+
   desc "Vim-flavored modal terminal editor with batteries included"
   homepage "https://docs.vorto-editor.dev/"
   license any_of: ["MIT", "Apache-2.0"]
 
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
   on_macos do
     on_arm do
-      url "https://github.com/vorto-editor/vorto/releases/download/v0.14.0/vorto-0.14.0-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/vorto-editor/vorto/releases/download/v#{VORTO_VERSION}/vorto-#{VORTO_VERSION}-aarch64-apple-darwin.tar.gz"
       sha256 "14e15146cae8801b6055f36c739599fdb1253a756fda458b5b241a240c35e0ae"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/vorto-editor/vorto/releases/download/v0.14.0/vorto-0.14.0-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/vorto-editor/vorto/releases/download/v#{VORTO_VERSION}/vorto-#{VORTO_VERSION}-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "c9ceb953cc9bc3594a8a04f591921e20ca306c56002a6319a455365d008a5872"
     end
     on_intel do
-      url "https://github.com/vorto-editor/vorto/releases/download/v0.14.0/vorto-0.14.0-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/vorto-editor/vorto/releases/download/v#{VORTO_VERSION}/vorto-#{VORTO_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "0911fcc749bf1bbf130d3233b86e171ca256ec3f85dfe987f575ac4f73c1b2f3"
     end
   end
